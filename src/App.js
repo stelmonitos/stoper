fd1453c91377916f0e5e1b4233091c53e5c89125
Jakub Stelmasiak <stelmon3@gmail.com>
2024-02-07 19:58:00 +0100

    raw stoper

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import { useState } from 'react';
 import FormattedTime from './components/FormattedTime';
+import { useState, useEffect } from 'react';
 
 function App() {
     const [time, setTime] = useState(0);
     const [intervalId, setIntervalId] = useState(null);
 
+    useEffect(() => {
+        return () => {
+            clearInterval(intervalId);
+        };
+    }, [intervalId]);
+
     const startTimer = () => {
         if (intervalId) return;
         const id = setInterval(() => {
-            setTime(prevTime => prevTime + 10);
-        }, 10);
+            setTime(prevTime => prevTime + 1);
+        }, 1);
         setIntervalId(id);
     };
-    
+
+    const stopTimer = () => {
+        clearInterval(intervalId);
+        setIntervalId(null);
+    };
+
+    const resetTimer = () => {
+        clearInterval(intervalId);
+        setIntervalId(null);
+        setTime(0);
+    }
+
   return(
       <div>
       <FormattedTime time={time} />
       <button onClick={startTimer}>Start</button>
-      <button>Stop</button>
-      <button>Reset</button>
+      <button onClick={stopTimer}>Stop</button>
+      <button onClick={resetTimer}>Reset</button>
       </div>
   );
 
